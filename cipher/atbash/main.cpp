9b314c8f2b49de7dc8133c1b825f31b876147d44
Jose <no-email-set>
2021-08-24 19:16:24 +0200

    add atbash use example in the atbash/main directory

--- cipher/atbash/main.cpp
+++ cipher/atbash/main.cpp
@@ -5,16 +5,16 @@
 int main()
 {
     const std::string message = "HELLO WORLD";
-    std::cout << "atbash::encrypt(): " << "message.length(): " << message.length() << ", message: " << message << ", message[0]: " << message[0] << std::endl;
+    std::cout << "main: " << std::endl << " - Original message:" << std::endl << "  - Length: " << message.length() << std::endl << "  - Message: " << message << std::endl << "  - Message[0]: " << message[0] << std::endl;
 
     // Atbash *atbash = new Atbash();
     Atbash atbash;
-    int key = 1;
-    atbash.setKey(key);
+    // std::string key = "CDEFGHIJKLMNOPQRSTUVWXYZ0123456789AB";
+    // atbash.setKey(key);
     atbash.encrypt(&message);
 
-    std::cout << "main::atbash.getEncryptedMessage(): " << atbash.getEncryptedMessage() << std::endl;
-    std::cout << "main::atbash.getKey(): " << atbash.getKey() << std::endl;
+    std::string encryptedMessage = atbash.getEncryptedMessage();
+    std::cout << " - Encrypted message: " << std::endl << "  - Message: " << encryptedMessage << std::endl << "  - Length: " << encryptedMessage.length() << std::endl << "  - Encryption Key: \"" << atbash.getKey() << "\"" << std::endl;
 
     return 0;
 }
